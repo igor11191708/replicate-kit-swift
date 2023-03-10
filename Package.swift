eaa4deb60a260270ab3544029ec2dd6bc9620233
Igor <igorshelopaev@gmail.com>
2023-03-10 19:47:53 +0500

    update

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
             targets: ["replicate-kit-swift"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/The-Igor/async-http-client.git", .branch("main"))
+        .package(url: "https://github.com/The-Igor/async-http-client.git", .branchItem("main"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
