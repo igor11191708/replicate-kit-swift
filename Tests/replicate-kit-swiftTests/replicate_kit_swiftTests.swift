3be1c514ffa902af3f18c33b3a9ae9ab44538b28
Igor <igorshelopaev@gmail.com>
2024-06-25 17:07:03 +0200

    Update replicate_kit_swiftTests.swift

--- Tests/replicate-kit-swiftTests/replicate_kit_swiftTests.swift
+++ Tests/replicate-kit-swiftTests/replicate_kit_swiftTests.swift
@@ -3,9 +3,6 @@ import XCTest
 
 final class replicate_kit_swiftTests: XCTestCase {    
     func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        // XCTAssertEqual(replicate_kit_swift().text, "Hello, World!")
+
     }
 }
